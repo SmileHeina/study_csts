dc5e725eef842b042fc523dc80cb8787506598c5
SmileHeina <smilehyeeun@gmail.com>
2025-03-28 14:07:03 +0900

    Create cstsfl_2501_quizData_test.inc.js

---
+++ static/js/cstsfl_2501_quizData_test.inc.js
@@ -0,0 +1,91 @@
+//CSTS FL 2501
+var cstsfl_2501_quizData_test = [
+{
+    "type": "check",
+    "question": "소프트웨어 개발 단계별 테스트에 대한 설명으로 올바르지 않은 것은 무엇인가?",
+    "explanation": "",
+    "options": [
+           {"value": "aaaaaa",  "right": true}, 
+           {"value": "bbbbbbbb",  "right": true},
+           {"value": "vvvvv",  "right": false}, 
+           {"value": "ddddddddd",  "right": false}
+    ],
+    "correct": [0, 1]
+},
+{
+    "type": "radio",
+    "question": "명세 기반 테스트 기법 중에서 경계값 분석은 동등분할 테스트보다 테스트 강도가 높다.",
+    "explanation": "",
+    "options": [
+           {"value": "○",  "right": true}, 
+           {"value": "X",  "right": false}
+    ],
+    "correct": 0
+},
+{
+    "type": "text",
+    "question": "다음 보기에서 설명하는 경험기반 테스트 기법은 무엇인가?[FL-2501-62.jpg]",
+    "explanation": "오류 추정(Error Guessing)",
+    "correct": ["오류 추정","Error Guessing"]
+},
+{
+    "type": "text",
+    "question": "시스템 처리 능력 이상의 부하를 가하여 비정상적인 상황에서의 처리가 적절히 수행되는지 확인하는 테스트를 무엇이라 하는가? (       ) 테스트",
+    "explanation": "스트레스, Stress",
+    "correct": ["스트레스", "Stress"]
+},
+{ 
+    "type": "radio",
+    "question": "<보기>는 글꼴 변경 기능이 개발 완료 된 후, 품질 특성을 고려한 테스팅을 통해 수정 전과 수정 후의 모습이다. 이 때 고려된 품질 특성으로 가장 적절한 것은?[FL-2501-01.jpg]",
+    "explanation": "",
+    "options": [
+           {"value": "사용성",  "right": true}, 
+           {"value": "호환성",  "right": false}, 
+           {"value": "유지보수성",  "right": false}, 
+           {"value": "신뢰성",  "right": false}
+           ],
+    "correct": 0
+},
+{
+    "type": "text",
+    "question": "다음 괄호 안에 들어갈 알맞은 테스팅 방법을 기술하시오.[FL-2501-64.jpg]",
+    "explanation": "재테스팅(Re-testing)",
+    "correct": ["재테스팅", "Re-testing"]
+},
+{
+    "type": "text",
+    "question": "<보기>는 인스펙션 실행 순서이다. 빈칸에 들어갈 절차는 무엇인가?[FL-2501-65.jpg]",
+    "explanation": "재작업, rework",
+    "options": [
+        {"value": "정답 확인", "right": true}
+    ],
+    "correct": 0
+},
+{
+    "type": "text",
+    "question": "다음의 제어 흐름도를 갖는 프로그램에 A, B, C, D 경로를 테스트 케이스로 정의하고 테스트를 수행하였을 때, 문장 커버리지는 얼마인가?[FL-2501-66.jpg]",
+    "explanation": "100(%)",
+    "correct": "100(%)"
+},
+{
+    "type": "text",
+    "question": "<보기>의 수식은 (      ) 커버리지를 구하기 위한 계산법이다.[FL-2501-67.jpg]",
+    "explanation": "다중 조건",
+    "correct": "다중 조건
+},
+{
+    "type": "radio",
+    "question": "다음 괄호 안에 알맞은 테스트 활동을 기재하시오.[FL-2501-68.jpg]",
+    "explanation": "테스트 종료/ 완료",
+    "options": [
+        {"value": "정답 확인", "right": true}
+    ],
+    "correct": 0
+},
+{
+    "type": "text",
+    "question": "다음 테스트 환경구축 및 관리 활동에서 아래에 해당되는 산출물의 명칭을 기재하시오.[FL-2501-69.jpg]",
+    "explanation": "테스트 환경 준비 보고서",
+    "correct": "테스트 환경 준비 보고서"
+},
+];
