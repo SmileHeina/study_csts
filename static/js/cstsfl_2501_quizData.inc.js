b0995e0be8e53af29530e0c71b0905a08bb9e01e
SmileHeina <smilehyeeun@gmail.com>
2025-04-01 13:53:24 +0900

    Update cstsfl_2501_quizData.inc.js

--- static/js/cstsfl_2501_quizData.inc.js
+++ static/js/cstsfl_2501_quizData.inc.js
@@ -707,49 +707,49 @@ var cstsfl_2501_quizData = [
         "correct": ["디버깅", "Debugging", "debugging"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "다음 보기에서 설명하는 경험기반 테스트 기법은 무엇인가?[FL-2501-62.jpg]",
         "explanation": "오류 추정 | Error Guessing",
         "correct": ["오류 추정", "Error Guessing", "error guessing"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "시스템 처리 능력 이상의 부하를 가하여 비정상적인 상황에서의 처리가 적절히 수행되는지 확인하는 테스트를 무엇이라 하는가? (       ) 테스트",
         "explanation": "스트레스 | Stress",
         "correct": ["스트레스", "Stress", "stress"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "다음 괄호 안에 들어갈 알맞은 테스팅 방법을 기술하시오.[FL-2501-64.jpg]",
         "explanation": "재테스팅 | Re-testing",
         "correct": ["재테스팅", "Re-testing"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "<보기>는 인스펙션 실행 순서이다. 빈칸에 들어갈 절차는 무엇인가?[FL-2501-65.jpg]",
         "explanation": "재작업 | Rework",
         "correct": ["재작업", "rework", "Rework"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "다음의 제어 흐름도를 갖는 프로그램에 A, B, C, D 경로를 테스트 케이스로 정의하고 테스트를 수행하였을 때, 문장 커버리지는 얼마인가?[FL-2501-66.jpg]",
         "explanation": "100(%)",
         "correct": "100%"
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "<보기>의 수식은 (      ) 커버리지를 구하기 위한 계산법이다.[FL-2501-67.jpg]",
         "explanation": "다중 조건",
         "correct": "다중 조건건"
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "다음 괄호 안에 알맞은 테스트 활동을 기재하시오.[FL-2501-68.jpg]",
         "explanation": "테스트 종료 | 테스트 완료",
         "correct": ["테스트 종료", "테스트 완료"]
     },
     {
-        "type": "radio",
+        "type": "text",
         "question": "다음 테스트 환경구축 및 관리 활동에서 아래에 해당되는 산출물의 명칭을 기재하시오.[FL-2501-69.jpg]",
         "explanation": "테스트 환경 준비 보고서",
         "correct": "테스트 환경 준비 보고서"
